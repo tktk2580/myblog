88955597df1162ee46e3f24647d263e794fb2a94
EC2 Default User <ec2-user@ip-172-31-36-206.us-east-2.compute.internal>
2020-03-09 12:01:04 +0000

    up to

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,5 @@
 class Comment < ApplicationRecord
   belongs_to :post
-  validates :body, presence: true
+  validates :body, presence: true, length:{maximum:25, message: 'Up to 25 characters'}
   # r23
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -4,6 +4,6 @@ class Post < ApplicationRecord
   # r23記事とコメントが１対nの関係だからhas_many
   #r23 @post.commentsポストからコメントを持ってこれる
   validates :title, presence: true, length:{minimum: 3, message: 'Too short to post!'}
-  validates :body, presence: true
+  validates :body, presence: true, length:{maximum:700, message: 'Up to 700 characters'}
   
 end
